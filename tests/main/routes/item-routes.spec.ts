ec03eabaa79d5c028285d3d16abeb16bd7727061
Douglas <douglas-cprdev@hotmail.com>
2022-03-15 22:08:10 -0300

    (tests) route: load item by id

--- tests/main/routes/item-routes.spec.ts
+++ tests/main/routes/item-routes.spec.ts
@@ -47,5 +47,18 @@ describe('Item Routers', () => {
       expect(httpResponse.body).toHaveProperty('description')
       expect(httpResponse.body).toHaveProperty('damage')
     })
-  })
+  }),
+    describe('GET api/item/:id', () => {
+      it('should return status 200 on success', async () => {
+        const httpResponse = await request(app).get('/api/item/1')
+
+        expect(httpResponse.status).toBe(200)
+      })
+
+      it('should return status code 404 if not found', async () => {
+        const httpResponse = await request(app).get('/api/item/any_id')
+
+        expect(httpResponse.status).toBe(404)
+      })
+    })
 })
